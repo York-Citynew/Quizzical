05f75c31b7b4767e40737ae1be67bfb4badf2051
York-Citynew <y.c.new.a@gmail.com>
2022-10-25 21:33:25 +0330

    minor improvements

--- src/components/QB.js
+++ src/components/QB.js
@@ -1,92 +1,7 @@
-// import { useState } from "react"
-
 import { useState } from "react"
 
-
-// export default function QB(props) {
-
-//     const [choiceObjs, setChoiceObjs] = useState(
-//         [
-//             {choice: props.CA,
-//              isTrue: true, 
-//              isSelected: false
-//             },
-//             ...props.WAs.map(
-//                 element=>({
-//                     choice: element, 
-//                     isTrue: false, 
-//                     isSelected: false
-//                 }))
-//         ].sort((a, b) => 0.5 - Math.random())
-//     )
-
-//     function optionStateChange(event) {
-//         if(!props.showAnswer) {
-//             setChoiceObjs(
-//                 prev=> {
-//                     const newArray = []
-//                     for (let i =0;i< prev.length;i++) {
-//                         if(prev[i].choice === event.target.id) {
-//                             newArray.push(
-//                                 {...prev[i],
-//                                  isSelected: !prev[i].isSelected,
-//                             })
-//                         }
-//                         else {
-//                             newArray.push({...prev[i], isSelected: false})
-//                         }
-//                     }
-//                     return newArray
-//                 }
-//         )
-//     }
-//     }
-
-//     const choiceEls=
-//         choiceObjs.map(
-//             element=>
-//                 <label
-//                     key={element.choice}
-                    // className={
-                    //     `input--${
-                    //         props.showAnswer?
-                    //         (element.isTrue?"correct":"wrong"):
-                    //         element.isSelected?"select":"normal"
-                    //     }`}
-//                     htmlFor={element.choice}>
-//                     <input
-//                         type="radio"
-//                         id={element.choice}
-//                         name={props.question}
-//                         checked={element.isSelected}
-                        // onChange={
-                        //     (event)=>{
-                        //         optionStateChange(event);
-                        //         props.handleAnswersTrucy(
-                        //             props.question,
-                        //             choiceObjs.some(
-                        //                 obj=>obj.isTrue && obj.isSelected
-                        //                 )
-                        //                 )
-                        //     }
-                        // }
-//                         />
-//                     {element.choice}
-//                 </label>
-//             )
-//     return (
-//         <div>
-//             <h3>{props.question}</h3>
-//             <div className="options-con display--flex">
-//                 {choiceEls}
-//             </div>
-//             <hr />
-//         </div>
-//     )
-// }
-
 export default function QB(props) {
-
+    
     const [choiceObjs, setChoiceObjs] = useState(
         [
             props.CA,
@@ -96,23 +11,23 @@ export default function QB(props) {
                 choice: element,
                 isSelected: false
             })).sort((a, b) => 0.5 - Math.random())
-    )
-
-    function handleSetChoiceObjs(choice) {
-        setChoiceObjs(prev=> {
-            const newArr = []
+            )
+            
+            function handleSetChoiceObjs(choice) {
+                setChoiceObjs(prev=> {
+                    const newArr = []
             for (let i =0; i< prev.length; i++) {
                 (choice === prev[i].choice)?(newArr.push({...prev[i], isSelected: !prev[i].isSelected})):(newArr.push({...prev[i], isSelected: false}))
             }
             return newArr
         })
     }
-
+    
     const choiceEls=
         choiceObjs.map(
             choice=>
-                <label
-                    key={choice.choice}
+            <label
+            key={choice.choice}
 
 
                     className={
@@ -130,7 +45,7 @@ export default function QB(props) {
                             (event)=>{
                                 if (!props.showAnswer) {
                                 handleSetChoiceObjs(choice.choice);
-                                props.handleSetAnswersTrucy(
+                                props.handleQuiz(
                                     props.question,
                                     event.target.checked && choice.choice === props.CA
                                         )
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,118 +1,3 @@
-// import { useEffect, useState } from "react"
-// import QB from "./QB"
-// import Dark from "./Dark"
-
-// export default function Quiz(props) {
-//     //????????????
-//     const [data, setData] = useState([])
-            
-//             const [showAnswer, setShowAnswer] = useState(false)
-            
-//             const [receivedAnswers, setReceivedAnswers] = useState([])
-            
-            
-//             useEffect(()=>{
-//                 fetch("https://opentdb.com/api.php?amount=5&category=15&type=multiple")
-//                 .then(res=>res.json())
-//                 .then(res=>setData(res.results))
-//             }, [])
-            
-            // const [answersTrucy, setAnswersTrucy] = useState(
-            //         data.map(
-            //                 element=>(
-            //             {question: element.question,
-            //              isAnsweredCorrect: false
-            //             }
-            //         ))
-            //     )
-            
-            // useEffect(()=>
-            //     setAnswersTrucy(
-            //             data.map(
-            //                     element=>
-            //                     (
-            //                             {question: element.question,
-            //                              isAnsweredCorrect: false
-            //                             }
-            //                         ))
-            //                 )
-            //             ,[data])
-            
-    //         function handleAnswersTrucy(question, boolean) {
-    //             setAnswersTrucy(
-    //                     function (prev) {
-    //                         const newArray = []
-    //                         for (let i=0; i< prev.length; i++) {
-    //                             if(prev[i].question === question) {
-    //                                 newArray.push({...prev[i], isAnsweredCorrect: boolean})
-    //                             }
-    //                             else {
-    //                                 newArray.push(prev[i])
-    //                             }
-    //                         }
-    //                         return newArray
-    //                     }
-    //     )
-    // }
-
-//             const QBEls=
-//             data.map(
-//                 element=>
-//                 <QB 
-//                 key={element.question}
-//                 showAnswer={showAnswer}
-//                 question={element.question}
-//                 WAs={element.incorrect_answers}
-//                 CA={element.correct_answer}
-//                 handleAnswersTrucy={handleAnswersTrucy}
-//                 />    
-//                 )
-                
-                
-//                 function finalFunc() {
-//         if (!showAnswer) {
-//             setShowAnswer(prev=>!prev)
-//         }
-//         else (
-//             props.startFunc()
-//             )
-//             // !showAnswer?(setShowAnswer(prev=>!prev)):props.startFunc()
-//         }
-//         function countTrueAnswers() {
-//             const correctOnes = 
-//             answersTrucy.filter(
-//             element=>element.isAnsweredCorrect
-//             )
-//     return correctOnes.length
-// }
-        
-//         return (
-//             <div className="con-quiz width align--center display--flex">
-//             <Dark 
-//                 isDark={props.isDark}
-//                 handleDark={props.handleDark}
-//                 />
-//             {QBEls}
-//             <div className="con-result align--center display--flex">
-//                 {showAnswer && 
-//                     <p className="result">
-//                         You scored 
-//                         {countTrueAnswers()}/{QBEls.length}
-//                         correct answers
-//                     </p>
-//                 }
-//                 <button 
-//                     className="button--normal" 
-//                     id="that-button" 
-//                     onClick={finalFunc}>
-//                         {showAnswer?"New game":"submit answers"}
-//                 </button>
-//             </div>
-//         </div>
-//     )
-// }
-
-
 import { useEffect, useState } from "react"
 import QB from "./QB"
 import Dark from "./Dark"
@@ -121,36 +6,36 @@ export default function Quiz(props) {
     const [data, setData] = useState([])
             
     const [showAnswer, setShowAnswer] = useState(false)
-
-    const [answersTrucy, setAnswersTrucy] = useState(
-        data.map(
-                question=>(
-            {
-             question: question.question,
-             isAnsweredCorrect: false
-            }
-        ))
-    )
-            
             
             useEffect(()=>{
                 fetch("https://opentdb.com/api.php?amount=5&category=15&type=multiple")
                 .then(res=>res.json())
-                .then(res=>setData(res.results))
+                .then(res=>setData(res.results.map(
+                    element=>(
+                        {
+                            ...element,
+                            isAnsweredCorrect: false
+                        }
+                    )
+                )))
             }, [])
 
-            useEffect(()=>
-            setAnswersTrucy(
-                    data.map(
-                            element=>
-                            (
-                                    {question: element.question,
-                                     isAnsweredCorrect: false
-                                    }
-                                ))
-                        )
-                    ,[data])
-
+            function handleQuiz(question, boolean) {
+                setData(
+                    function (prev) {
+                        const newArray = []
+                        for (let i=0; i< prev.length; i++) {
+                            if(prev[i].question === question) {
+                                newArray.push({...prev[i], isAnsweredCorrect: boolean})
+                            }
+                            else {
+                                newArray.push(prev[i])
+                            }
+                        }
+                        return newArray
+                    }
+                )
+            }
 
             const QBEls=
             data.map(
@@ -161,33 +46,14 @@ export default function Quiz(props) {
                 question={element.question}
                 CA={element.correct_answer}
                 WAs={element.incorrect_answers}
-                handleSetAnswersTrucy={handleSetAnswersTrucy}
+                handleQuiz={handleQuiz}
                 />    
                 )
 
-                function handleSetAnswersTrucy(question, boolean) {
-                    setAnswersTrucy(
-                            function (prev) {
-                                const newArray = []
-                                for (let i=0; i< prev.length; i++) {
-                                    if(prev[i].question === question) {
-                                        newArray.push({...prev[i], isAnsweredCorrect: boolean})
-                                    }
-                                    else {
-                                        newArray.push(prev[i])
-                                    }
-                                }
-                                return newArray
-                            }
-            )
-        }
-
         function countTrueAnswers() {
-            const correctOnes = 
-            answersTrucy.filter(
-            element=>element.isAnsweredCorrect
-            )
-    return correctOnes.length
+    return data.filter(
+                element=>element.isAnsweredCorrect
+                ).length
 }
                 
                 
@@ -223,6 +89,7 @@ export default function Quiz(props) {
                         {showAnswer?"New game":"submit answers"}
                 </button>
             </div>
+            <button>asdsad</button>
         </div>
     )
 }
